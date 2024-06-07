6b3f95ad820aa6cd7c57b0bf597656ac6f45fda5
Saodat <saodat.riskiyeva@gmail.com>
2024-06-06 23:46:56 -0400

    creating and filling arrays

--- starter/script.js
+++ starter/script.js
@@ -435,3 +435,36 @@ btnSort.addEventListener('click', function (e) {
 
 // movements.sort((a, b) => b - a);
 // console.log(movements);
+
+const arrFill = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x);
+
+console.log(x.map(() => 5));
+
+// x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+arrFill.fill(47, 2, 6);
+console.log(arrFill);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('EUR', ''))
+  );
+
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
